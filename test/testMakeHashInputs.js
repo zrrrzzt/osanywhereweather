2a43ff0ce5fd4b010d228c95924304576828a9d3
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-01-03 00:44:07 +0100

    Initial commit

---
+++ test/testMakeHashInputs.js
@@ -0,0 +1,98 @@
+'use strict';
+
+var assert = require('assert')
+  , createHash = require('../lib/makehash')
+  ;
+
+describe('createHash - inputs', function(){
+
+  it('requires an opts object', function(done){
+
+    var opts = false
+      ;
+
+    createHash(opts, function(err, data){
+      assert.throws(function(){
+          if(err) throw err;
+        }, function(err){
+          if((err instanceof Error) && /Missing required input: options/.test(err)){
+            return true
+          }
+        },
+        "Unexpected error"
+      );
+      done();
+    });
+
+  });
+
+  it('requires opts.email to exist', function(done){
+
+    var opts = {
+        email: false
+      }
+      ;
+
+    createHash(opts, function(err, data){
+      assert.throws(function(){
+          if(err) throw err;
+        }, function(err){
+          if((err instanceof Error) && /Missing required param: options.email/.test(err)){
+            return true
+          }
+        },
+        "Unexpected error"
+      );
+      done();
+    });
+
+  });
+
+  it('requires opts.password to exist', function(done){
+
+    var opts = {
+        email: true,
+        password: false
+      }
+      ;
+
+    createHash(opts, function(err, data){
+      assert.throws(function(){
+          if(err) throw err;
+        }, function(err){
+          if((err instanceof Error) && /Missing required param: options.password/.test(err)){
+            return true
+          }
+        },
+        "Unexpected error"
+      );
+      done();
+    });
+
+  });
+
+  it('requires opts.challenge to exist', function(done){
+
+    var opts = {
+        email: true,
+        password: true,
+        challenge: false
+      }
+      ;
+
+    createHash(opts, function(err, data){
+      assert.throws(function(){
+          if(err) throw err;
+        }, function(err){
+          if((err instanceof Error) && /Missing required param: options.challenge/.test(err)){
+            return true
+          }
+        },
+        "Unexpected error"
+      );
+      done();
+    });
+
+  });
+
+});
