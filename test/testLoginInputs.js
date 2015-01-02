5d6e57ba81ef763d0d32277549bbb0084a46e056
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-01-03 00:11:40 +0100

    Initial commit

---
+++ test/testLoginInputs.js
@@ -0,0 +1,74 @@
+'use strict';
+
+var assert = require('assert')
+  , login = require('../lib/login')
+  ;
+
+describe('login - inputs', function(){
+
+  it('requires an opts object', function(done){
+
+    var opts = false
+      ;
+
+    login(opts, function(err, data){
+      assert.throws(function(){
+          if(err) throw err;
+        }, function(err){
+          if((err instanceof Error) && /Missing required input: options/.test(err)){
+            return true
+          }
+        },
+        "Unexpected error"
+      );
+      done();
+    });
+
+  });
+
+  it('requires opts.email to exist', function(done){
+
+    var opts = {
+          email: false
+        }
+      ;
+
+    login(opts, function(err, data){
+      assert.throws(function(){
+          if(err) throw err;
+        }, function(err){
+          if((err instanceof Error) && /Missing required param: options.email/.test(err)){
+            return true
+          }
+        },
+        "Unexpected error"
+      );
+      done();
+    });
+
+  });
+
+  it('requires opts.password to exist', function(done){
+
+    var opts = {
+        email: true,
+        password: false
+      }
+      ;
+
+    login(opts, function(err, data){
+      assert.throws(function(){
+          if(err) throw err;
+        }, function(err){
+          if((err instanceof Error) && /Missing required param: options.password/.test(err)){
+            return true
+          }
+        },
+        "Unexpected error"
+      );
+      done();
+    });
+
+  });
+
+});
