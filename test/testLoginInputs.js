79fb2318a4aeb8c7530a531afc8c24be79153395
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-02-01 19:35:30 +0100

    Fixes code style

--- test/testLoginInputs.js
+++ test/testLoginInputs.js
@@ -3,21 +3,21 @@
 var assert = require('assert');
 var login = require('../lib/login');
 
-describe('login - inputs', function(){
+describe('login - inputs', function() {
 
-  it('requires an opts object', function(done){
+  it('requires an opts object', function(done) {
 
     var opts = false;
 
-    login(opts, function(err, data){
-      assert.throws(function(){
-          if(err) {
+    login(opts, function(err, data) {
+      assert.throws(function() {
+          if (err) {
             throw err;
           } else {
             console.log(data);
           }
-        }, function(err){
-          if((err instanceof Error) && /Missing required input: options/.test(err)){
+        }, function(err) {
+          if ((err instanceof Error) && /Missing required input: options/.test(err)) {
             return true;
           }
         },
@@ -28,21 +28,21 @@ describe('login - inputs', function(){
 
   });
 
-  it('requires opts.email to exist', function(done){
+  it('requires opts.email to exist', function(done) {
 
     var opts = {
       email: false
     };
 
-    login(opts, function(err, data){
-      assert.throws(function(){
-          if(err) {
+    login(opts, function(err, data) {
+      assert.throws(function() {
+          if (err) {
             throw err;
           } else {
             console.log(data);
           }
-        }, function(err){
-          if((err instanceof Error) && /Missing required param: options.email/.test(err)){
+        }, function(err) {
+          if ((err instanceof Error) && /Missing required param: options.email/.test(err)) {
             return true;
           }
         },
@@ -53,22 +53,22 @@ describe('login - inputs', function(){
 
   });
 
-  it('requires opts.password to exist', function(done){
+  it('requires opts.password to exist', function(done) {
 
     var opts = {
       email: true,
       password: false
     };
 
-    login(opts, function(err, data){
-      assert.throws(function(){
-          if(err) {
+    login(opts, function(err, data) {
+      assert.throws(function() {
+          if (err) {
             throw err;
           } else {
             console.log(data);
           }
-        }, function(err){
-          if((err instanceof Error) && /Missing required param: options.password/.test(err)){
+        }, function(err) {
+          if ((err instanceof Error) && /Missing required param: options.password/.test(err)) {
             return true;
           }
         },
