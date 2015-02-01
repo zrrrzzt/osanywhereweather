8feedbb544f2cb5dc92b65ee97ecd8a72ef16346
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-02-01 19:23:04 +0100

    Fixes code style

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
 'use strict';
 
 var config = {
-      'API_URL' : 'http://api.osanywhereweather.com',
-      'API_VERSION' : '4'
-    }
-  ;
+  'API_URL' : 'http://api.osanywhereweather.com',
+  'API_VERSION' : '4'
+};
 
 module.exports = config;
