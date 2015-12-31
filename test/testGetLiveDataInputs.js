1e7cabbd7617b9d9e7b18249c900936a9fb4fac5
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-12-31 16:48:45 +0100

    Updates test to tap

--- test/testGetLiveDataInputs.js
+++ test/testGetLiveDataInputs.js
@@ -1,75 +1,36 @@
 'use strict'
 
-var assert = require('assert')
+var tap = require('tap')
 var getLiveData = require('../lib/getlivedata')
 
-describe('getLiveData - inputs', function () {
-  it('requires an opts object', function (done) {
-
-    var opts = false
-
-    getLiveData(opts, function (err, data) {
-      assert.throws(function () {
-        if (err) {
-          throw err
-        } else {
-          console.log(data)
-        }
-      }, function(err) {
-        if ((err instanceof Error) && /Missing required input: options/.test(err)) {
-          return true
-        }
-      },
-        'Unexpected error'
-      )
-      done()
-    })
+tap.test('requires an options object', function (test) {
+  var options = false
+  var expectedErrorMessage = 'Missing required input: options'
+  getLiveData(options, function (error, data) {
+    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
   })
+})
 
-  it('requires opts.stationId to exist', function (done) {
-    var opts = {
-      stationId: false
-    }
-
-    getLiveData(opts, function (err, data) {
-      assert.throws(function () {
-        if (err) {
-          throw err
-        } else {
-          console.log(data)
-        }
-      }, function (err) {
-        if ((err instanceof Error) && /Missing required param: options.stationId/.test(err)) {
-          return true
-        }
-      },
-        'Unexpected error'
-      )
-      done()
-    })
+tap.test('requires opts.stationId to exist', function (test) {
+  var options = {
+    stationId: false
+  }
+  var expectedErrorMessage = 'Missing required param: options.stationId'
+  getLiveData(options, function (error, data) {
+    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
   })
+})
 
-  it('requires opts.sessionKey to exist', function (done) {
-    var opts = {
-      stationId: true,
-      sessionKey: false
-    }
-
-    getLiveData(opts, function (err, data) {
-      assert.throws(function() {
-        if (err) {
-          throw err
-        } else {
-          console.log(data)
-        }
-      }, function (err) {
-        if ((err instanceof Error) && /Missing required param: options.sessionKey/.test(err)) {
-          return true;
-        }
-      },
-        'Unexpected error'
-      )
-      done()
-    })
+tap.test('requires opts.sessionKey to exist', function (test) {
+  var options = {
+    stationId: true,
+    sessionKey: false
+  }
+  var expectedErrorMessage = 'Missing required param: options.sessionKey'
+  getLiveData(options, function (error, data) {
+    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
   })
 })
