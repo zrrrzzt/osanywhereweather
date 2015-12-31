7428aa8f0816d6bf29105481300a62c0e0a8d1ab
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-12-31 16:43:54 +0100

    Updates test to use tap

--- test/testGetHistoryDataInputs.js
+++ test/testGetHistoryDataInputs.js
@@ -1,154 +1,78 @@
 'use strict'
 
-var assert = require('assert')
+var tap = require('tap')
 var getHistoryData = require('../lib/gethistorydata')
 
-describe('getHistoryData - inputs', function () {
-
-  it('requires an options object', function (done) {
-
-    var options = false
-
-    getHistoryData(options, function (err, data) {
-      assert.throws( function() {
-          if (err) {
-            throw err
-          } else {
-            console.log(data)
-          }
-        }, function(err) {
-          if ((err instanceof Error) && /Missing required input: options/.test(err)) {
-            return true
-          }
-        },
-        'Unexpected error'
-      )
-      done()
-    })
+tap.test('requires an options object', function (test) {
+  var options = false
+  var expectedErrorMessage = 'Missing required input: options'
+  getHistoryData(options, function (error, data) {
+    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
   })
+})
 
-  it('requires options.stationId to exist', function (done) {
-    var options = {
-      stationId: false
-    }
-
-    getHistoryData(options, function (err, data) {
-      assert.throws(function () {
-        if (err) {
-          throw err
-        } else {
-          console.log(data)
-        }
-      }, function (err) {
-        if ((err instanceof Error) && /Missing required param: options.stationId/.test(err)) {
-          return true
-        }
-      },
-        'Unexpected error'
-      )
-      done()
-    })
+tap.test('requires options.stationId to exist', function (test) {
+  var options = {
+    stationId: false
+  }
+  var expectedErrorMessage = 'Missing required param: options.stationId'
+  getHistoryData(options, function (error, data) {
+    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
   })
+})
 
-  it('requires options.sessionKey to exist', function(done) {
-    var options = {
-      stationId: true,
-      sessionKey: false
-    };
-
-    getHistoryData(options, function (err, data) {
-      assert.throws(function() {
-        if (err) {
-          throw err
-        } else {
-          console.log(data)
-        }
-      }, function(err) {
-        if ((err instanceof Error) && /Missing required param: options.sessionKey/.test(err)) {
-          return true
-        }
-      },
-        'Unexpected error'
-      )
-      done()
-    })
+tap.test('requires options.sessionKey to exist', function (test) {
+  var options = {
+    stationId: true,
+    sessionKey: false
+  }
+  var expectedErrorMessage = 'Missing required param: options.sessionKey'
+  getHistoryData(options, function (error, data) {
+    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
   })
+})
 
-  it('requires options.type to exist', function (done) {
-    var options = {
-      stationId: true,
-      sessionKey: true,
-      type: false
-    }
-
-    getHistoryData(options, function (err, data) {
-      assert.throws(function () {
-        if (err) {
-          throw err
-        } else {
-          console.log(data)
-        }
-      }, function (err) {
-        if ((err instanceof Error) && /Missing required param: options.type/.test(err)) {
-          return true
-        }
-      },
-        'Unexpected error'
-      )
-      done()
-    })
+tap.test('requires options.type to exist', function (test) {
+  var options = {
+    stationId: true,
+    sessionKey: true,
+    type: false
+  }
+  var expectedErrorMessage = 'Missing required param: options.type'
+  getHistoryData(options, function (error, data) {
+    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
   })
+})
 
-  it('requires options.channel to exist', function (done) {
-    var options = {
-      stationId: true,
-      sessionKey: true,
-      type: true,
-      channel: false
-    }
-
-    getHistoryData(options, function (err, data) {
-      assert.throws(function () {
-        if (err) {
-          throw err
-        } else {
-          console.log(data)
-        }
-        }, function (err) {
-          if ((err instanceof Error) && /Missing required param: options.channel/.test(err)) {
-            return true
-          }
-        },
-        'Unexpected error'
-      )
-      done()
-    })
+tap.test('requires options.channel to exist', function (test) {
+  var options = {
+    stationId: true,
+    sessionKey: true,
+    type: true,
+    channel: false
+  }
+  var expectedErrorMessage = 'Missing required param: options.channel'
+  getHistoryData(options, function (error, data) {
+    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
   })
+})
 
-  it('requires options.duration to exist', function (done) {
-    var options = {
-      stationId: true,
-      sessionKey: true,
-      type: true,
-      channel: true,
-      duration: false
-    }
-
-    getHistoryData(options, function (err, data) {
-      assert.throws(function () {
-        if (err) {
-          throw err
-        } else {
-          console.log(data)
-        }
-      }, function(err) {
-        if ((err instanceof Error) && /Missing required param: options.duration/.test(err)) {
-          return true
-        }
-      },
-        'Unexpected error'
-      )
-      done()
-    })
+tap.test('requires options.duration to exist', function (test) {
+  var options = {
+    stationId: true,
+    sessionKey: true,
+    type: true,
+    channel: true,
+    duration: false
+  }
+  var expectedErrorMessage = 'Missing required param: options.duration'
+  getHistoryData(options, function (error, data) {
+    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
   })
 })
