5af01e4f63e27fc608762efd8f2dcf5d7f546e5e
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-01-01 12:02:18 +0100

    Adds env support to config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 'use strict'
 
 var config = {
-  'API_URL': 'http://api.osanywhereweather.com',
-  'API_VERSION': '4'
+  'API_URL': process.env.API_URL || 'http://api.osanywhereweather.com',
+  'API_VERSION': process.env.API_VERSION || '4'
 }
 
 module.exports = config
