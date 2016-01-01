4b83c15bdfa8f2529d82388d36a3cc6363437700
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-01-01 11:21:41 +0100

    Updates test to tap

--- test/testLoginInputs.js
+++ test/testLoginInputs.js
@@ -1,74 +1,36 @@
 'use strict'
 
-var assert = require('assert')
+var tap = require('tap')
 var login = require('../lib/login')
 
-describe('login - inputs', function () {
-  it('requires an opts object', function (done) {
-    var opts = false
-
-    login(opts, function (err, data) {
-      assert.throws( function () {
-        if (err) {
-          throw err
-        } else {
-          console.log(data)
-        }
-      }, function(err) {
-        if ((err instanceof Error) && /Missing required input: options/.test(err)) {
-          return true
-        }
-      },
-        'Unexpected error'
-      )
-      done()
-    })
+tap.test('requires an options object', function (test) {
+  var options = false
+  var expectedErrorMessage = 'Missing required input: options'
+  login(options, function (error, data) {
+    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
   })
+})
 
-  it('requires opts.email to exist', function (done) {
-    var opts = {
-      email: false
-    }
-
-    login(opts, function (err, data) {
-      assert.throws(function () {
-        if (err) {
-          throw err
-        } else {
-          console.log(data);
-        }
-      }, function (err) {
-        if ((err instanceof Error) && /Missing required param: options.email/.test(err)) {
-          return true
-        }
-      },
-        'Unexpected error'
-      )
-      done()
-    })
+tap.test('requires options.email to exist', function (test) {
+  var options = {
+    email: false
+  }
+  var expectedErrorMessage = 'Missing required param: options.email'
+  login(options, function (error, data) {
+    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
   })
+})
 
-  it('requires opts.password to exist', function (done) {
-    var opts = {
-      email: true,
-      password: false
-    }
-
-    login(opts, function (err, data) {
-      assert.throws(function () {
-        if (err) {
-          throw err
-        } else {
-          console.log(data)
-        }
-      }, function (err) {
-        if ((err instanceof Error) && /Missing required param: options.password/.test(err)) {
-          return true
-        }
-      },
-        'Unexpected error'
-      )
-      done()
-    })
+tap.test('requires options.password to exist', function (test) {
+  var options = {
+    email: true,
+    password: false
+  }
+  var expectedErrorMessage = 'Missing required param: options.password'
+  login(options, function (error, data) {
+    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
   })
 })
