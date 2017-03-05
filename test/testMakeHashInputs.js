a47815c2a8442c4ef3864485350fabfcf6b738e6
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-03-05 11:26:21 +0100

    More refactoring

--- test/testMakeHashInputs.js
+++ test/testMakeHashInputs.js
@@ -1,49 +1,45 @@
 'use strict'
 
-var tap = require('tap')
-var createHash = require('../lib/makehash')
+const tap = require('tap')
+const createHash = require('../lib/makehash')
 
-tap.test('requires an options object', function (test) {
-  var options = false
-  var expectedErrorMessage = 'Missing required input: options'
-  createHash(options, function (error, data) {
-    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
-    test.done()
-  })
-})
+tap.throws(
+  function () {
+    createHash()
+  },
+  {message: 'Missing required input: options'},
+  'requires an options object'
+)
 
-tap.test('requires options.email to exist', function (test) {
-  var options = {
-    email: false
-  }
-  var expectedErrorMessage = 'Missing required param: options.email'
-  createHash(options, function (error, data) {
-    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
-    test.done()
-  })
-})
+tap.throws(
+  function () {
+    createHash({
+      email: false
+    })
+  },
+  {message: 'Missing required param: options.email'},
+  'requires options.email to exist'
+)
 
-tap.test('requires options.password to exist', function (test) {
-  var options = {
-    email: true,
-    password: false
-  }
-  var expectedErrorMessage = 'Missing required param: options.password'
-  createHash(options, function (error, data) {
-    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
-    test.done()
-  })
-})
+tap.throws(
+  function () {
+    createHash({
+      email: true,
+      password: false
+    })
+  },
+  {message: 'Missing required param: options.password'},
+  'requires options.password to exist'
+)
 
-tap.test('requires opts.challenge to exist', function (test) {
-  var options = {
-    email: true,
-    password: true,
-    challenge: false
-  }
-  var expectedErrorMessage = 'Missing required param: options.challenge'
-  createHash(options, function (error, data) {
-    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
-    test.done()
-  })
-})
+tap.throws(
+  function () {
+    createHash({
+      email: true,
+      password: true,
+      challenge: false
+    })
+  },
+  {message: 'Missing required param: options.challenge'},
+  'requires options.challenge to exist'
+)
